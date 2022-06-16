d0a6257a703bc70acbf35309c7e6947ba93ad3b0
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-06-16 19:37:03 +1200

    comit before copy package.json from world project

---
+++ server/public/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./client/index.js":
+/*!*************************!*\
+  !*** ./client/index.js ***!
+  \*************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/sandbox1/Desktop/Saitama/client/index.js: Support for the experimental syntax 'jsx' isn't currently enabled (7:22):\n\n\u001b[0m \u001b[90m 5 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 6 |\u001b[39m document\u001b[33m.\u001b[39maddEventListener(\u001b[32m'DOMContentLoaded'\u001b[39m\u001b[33m,\u001b[39m () \u001b[33m=>\u001b[39m {\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 7 |\u001b[39m     \u001b[33mReactDOM\u001b[39m\u001b[33m.\u001b[39mrender( \u001b[33m<\u001b[39m \u001b[33mApp\u001b[39m \u001b[33m/\u001b[39m \u001b[33m>\u001b[39m \u001b[33m,\u001b[39m document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'app'\u001b[39m))\u001b[0m\n\u001b[0m \u001b[90m   |\u001b[39m                      \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 8 |\u001b[39m })\u001b[0m\n\nAdd @babel/preset-react (https://github.com/babel/babel/tree/main/packages/babel-preset-react) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-jsx) to the 'plugins' section to enable parsing.\n    at instantiate (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:72:32)\n    at constructor (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:358:12)\n    at Parser.raise (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:3341:19)\n    at Parser.expectOnePlugin (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:3398:18)\n    at Parser.parseExprAtom (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:13073:18)\n    at Parser.parseExprSubscripts (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:12643:23)\n    at Parser.parseUpdate (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:12622:21)\n    at Parser.parseMaybeUnary (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:12593:23)\n    at Parser.parseMaybeUnaryOrPrivate (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:12387:61)\n    at Parser.parseExprOps (/Users/sandbox1/Desktop/Saitama/node_modules/@babel/parser/lib/index.js:12394:23)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./client/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
